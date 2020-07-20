c5784eb41a70d54757fb479371b847944e5de3f1
Olaf Haalstra <olafhaalstra@gmail.com>
2020-07-20 21:24:31 +0200

    Added print statement for monetary account logging

--- app/bunq.py
+++ app/bunq.py
@@ -163,6 +163,7 @@ def retrieve_accounts(config):
     config["accounts"] = []
     result = get("v1/user/{}/monetary-account".format(config["user_id"]),
                  config)
+    print(result)
     for res in result["Response"]:
         for typ in res:
             acc = res[typ]
